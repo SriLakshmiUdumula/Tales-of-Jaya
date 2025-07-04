ee9aea680f447110b1668b47ea73228dcf26436c
SriLakshmiUdumula <sril32996@gmail.com>
2025-07-04 23:06:47 +0530

    initial commit

--- tales_of_jaya/settings.py
+++ tales_of_jaya/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
 
 
 # This should point to your static directory
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
