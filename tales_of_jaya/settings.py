fdcca8fe822fb79618cebce37ea1c039f325cbaa
SriLakshmiUdumula <sril32996@gmail.com>
2025-07-04 22:27:54 +0530

    initial commit

--- tales_of_jaya/settings.py
+++ tales_of_jaya/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^8#x%7#7my29lr5%n&9o3jdvamqp9-&5rqkmxcek%t(7rvncf2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tales_of_jaya.onrender.com']
 
 
 # Application definition
